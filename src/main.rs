6b1fc74379de009ab2e98372f2e38020886eed91
Ashwin Ramesh <ashkaty@gmail.com>
2024-01-30 13:57:58 -0600

    Dev Commit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io;
 use std::io::BufReader;
 use rustfft::{FftPlanner, num_complex::Complex};
 use std::path::Path;
+use minimp3::{Decoder, Frame, Error};
 use cute::c;
 
 
@@ -13,29 +14,15 @@ fn readFile(filePath:&Path) -> File{
 }
 
 
-fn parseReader(audioSample: File, fileType: &str) -> (audrey::Reader<BufReader<File>>, u32){
+// fn parseReader(audioSample: File, fileType: &str) -> (audrey::Reader<BufReader<File>>, u32){
 
-    let reader = io::BufReader::new(audioSample);
+    
+//     else if (fileType == "mp3"){
+//         return ()
+//     }
 
-
-    if (fileType == "flac"){
-        let (audreyReader, channels) = match Reader::new(reader) {
-            Ok(s) => {
-                let sReader = s;
-                let description = &sReader.description();
-                let channels = description.channel_count();
-                (sReader, channels)
-            },
-            Err(e) => panic!("{}", e) 
-        };
-        return (audreyReader, channels);
-    }
-    else if (fileType == "mp3"){
-        return ()
-    }
-
-    return (audreyReader, channels);
-}
+//     return (audreyReader, channels);
+// }
 
 
 fn mp3SampleArray(){
@@ -46,14 +33,51 @@ fn audreySampleArray(){
 
 }
 
+fn convertMP3ToComplex(audioSample: File){
+    
+    let mut reader = Decoder::new(audioSample);
+
+    println!("Channels: {}, Sample rate: {}", &reader.next_frame().unwrap().channels, &reader.next_frame().unwrap().sample_rate);
+    
+    
+    
+    
+    // let mut sampleArray = Vec::<f64>::new();
+
+    // let samples = audreyReader.samples::<f64>();
+
+    // for sample in samples {
+    //     sampleArray.push(sample.expect("Error at Pushing Sample Array"))
+    // } 
+
+    // let complexSamples: Vec<Complex<f64>> = c![Complex::new(x,0.0), for x in sampleArray];   
+
+    // return complexSamples;
+    return;
+}
 
 
-fn convertAudioToComplex(mut reader: Reader<BufReader<File>>, channels: u32) -> Vec<Complex<f64>>{
-    println!("{:?}", reader.description());
-
+fn convertNonMP3ToComplex(audioSample: File) -> Vec<Complex<f64>>{
+    
+    let reader = io::BufReader::new(audioSample);
+    
+    
+    let (mut audreyReader, channels) = match Reader::new(reader) {
+        Ok(s) => {
+            let sReader = s;
+            let description = &sReader.description();
+            let channels = description.channel_count();
+            (sReader, channels)
+        },
+        Err(e) => panic!("{}", e) 
+    };
+    
+    println!("{:?}", &audreyReader.description());
+    
+    
     let mut sampleArray = Vec::<f64>::new();
 
-    let samples = reader.samples::<f64>();
+    let samples = audreyReader.samples::<f64>();
 
     for sample in samples {
         sampleArray.push(sample.expect("Error at Pushing Sample Array"))
@@ -78,30 +102,35 @@ fn calculateMagnitude(complex: &Complex<f64>) -> f64 {
 
 
 fn main() {
-    let filePath = Path::new("C:/Users/ashka/Documents/Coding/Guitar_Tab_Generator/A2_mp3.Flac");     
+    let filePath = Path::new("/users/aramesh/Downloads/E.mp3");     
     let fileType = filePath.extension().unwrap().to_str().unwrap();
 
     let file = readFile(filePath);
     
-    let (audioSample, channels) = parseReader(file, fileType);
+    if (fileType != "mp3") {
+        convertNonMP3ToComplex(file);
+    }
+    else {
+        convertMP3ToComplex(file);
+    }
 
-    let complexSamples = convertAudioToComplex(audioSample, channels);
     
-    let complexSampleLength = &complexSamples.len();
+    
+    // let complexSampleLength = &complexSamples.len();
 
-    let fftTransform = performFFT(complexSamples);
+    // let fftTransform = performFFT(complexSamples);
 
-    let magnitudeArray = c![calculateMagnitude(&x), for x in fftTransform];
+    // let magnitudeArray = c![calculateMagnitude(&x), for x in fftTransform];
 
-    //what the fuck is this next function...it's the find the max value of the array WHY CAN'T I JUST DO magnitudeArray.max?!?!?!?
-    let maxMagnitude = magnitudeArray.iter().cloned().fold(0./0., f64::max);
-    println!("{}", maxMagnitude);
+    // //what the fuck is this next function...it's the find the max value of the array WHY CAN'T I JUST DO magnitudeArray.max?!?!?!?
+    // let maxMagnitude = magnitudeArray.iter().cloned().fold(0./0., f64::max);
+    // println!("{}", maxMagnitude);
 
-    let mut maxMagnitudeIter = magnitudeArray.iter();
+    // let mut maxMagnitudeIter = magnitudeArray.iter();
 
-    let maxMagnitudeIndex = maxMagnitudeIter.position(|&x| x == maxMagnitude).unwrap();
+    // let maxMagnitudeIndex = maxMagnitudeIter.position(|&x| x == maxMagnitude).unwrap();
 
-    let maxFrequency = (maxMagnitudeIndex) as f64 * 48000.0 / *(complexSampleLength) as f64;
+    // let maxFrequency = (maxMagnitudeIndex) as f64 * 48000.0 / *(complexSampleLength) as f64;
 
-    println!("{}", maxFrequency);
+    // println!("{}", maxFrequency);
 }
